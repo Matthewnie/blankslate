1dcf49d1bbd088c2222098b4e1d65440fc3482c3
Matthew Niedzielski <matt@ledigital.com>
2019-11-05 16:14:20 -0500

    format and add classes

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
-</div>
-<footer id="footer">
-<div id="copyright">
-&copy; <?php echo esc_html( date_i18n( __( 'Y', 'blankslate' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
-</div>
-</footer>
-</div>
-<?php wp_footer(); ?>
-</body>
+      </div>
+      <footer id="footer" class="site-footer">
+        <div id="copyright">
+        &copy; <?php echo esc_html( date_i18n( __( 'Y', 'blankslate' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
+        </div>
+      </footer>
+    </div>
+    <?php wp_footer(); ?>
+  </body>
 </html>
